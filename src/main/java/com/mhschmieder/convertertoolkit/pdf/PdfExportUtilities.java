2467783c21c54804cd4d02238c190ee1e645e489
Mark Schmieder <markhschmieder@gmail.com>
2020-08-03 22:46:29 -0700

    Correction of copyright headers referring to the wrong library due to copy/paste

--- src/main/java/com/mhschmieder/convertertoolkit/pdf/PdfExportUtilities.java
+++ src/main/java/com/mhschmieder/convertertoolkit/pdf/PdfExportUtilities.java
@@ -24,8 +24,7 @@
  * This file is part of the ConverterToolkit Library
  *
  * You should have received a copy of the MIT License along with the
- * FxConverterToolkit Library. If not, see
- * <https://opensource.org/licenses/MIT>.
+ * ConverterToolkit Library. If not, see <https://opensource.org/licenses/MIT>.
  *
  * Project: https://github.com/mhschmieder/convertertoolkit
  */
